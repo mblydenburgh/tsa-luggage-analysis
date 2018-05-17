5a040af5ea47238d5714f107cc4e0ddaf67daeb7
Mike Blydenburgh <36665948+mikebly@users.noreply.github.com>
2018-05-17 18:52:45 -0400

    Update app.js
    Successfully parse date info correctly to be able to determine incident month for analysis

--- app.js
+++ app.js
@@ -24,53 +24,66 @@ app.controller("LineCtrl", function ($scope,$http) {
       var first_sheet_name = workbook.SheetNames[0];
       /* Get worksheet */
       var worksheet = workbook.Sheets[first_sheet_name];
-      console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+      var jsonData = XLSX.utils.sheet_to_json(worksheet,{raw:true});
+      var headerNames = jsonData[0]["Incident Date"];
+      var date = XLSX.SSF.parse_date_code(headerNames,{date1904:false})
+      console.log(date);
+      console.log(date.m);
+      if(date.m == 1){
+        console.log("January");
+      };
+      //console.log(headerNames);
+      //console.log(XLSX.utils.sheet_to_json(worksheet,{raw:true}));
+
+      /* Button 1 click displays line graph */
+      $scope.graph1Ctrl = function(){
+        $scope.chart_type = "line";
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+      }
+      /* Button 2 click displays bar graph */
+      $scope.graph2Ctrl = function(){
+        $scope.chart_type = "bar";
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+          [85, 50, 74, 81, 75, 77, 69],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+      }
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+      $scope.options = {
+        scales: {
+          yAxes: [
+            {
+              id: 'y-axis-1',
+              type: 'linear',
+              display: true,
+              position: 'left'
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'right'
+            }
+          ]
+        }
+      };
+
+
     }
 
     oReq.send();
 
 
-  /* Button 1 click displays line graph */
-  $scope.graph1Ctrl = function(){
-    $scope.chart_type = "line";
-    $scope.labels = headerNames;
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-  }
-  /* Button 2 click displays bar graph */
-  $scope.graph2Ctrl = function(){
-    $scope.chart_type = "bar";
-    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['Series A', 'Series B'];
-    $scope.data = [
-      [85, 50, 74, 81, 75, 77, 69],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
-  }
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        },
-        {
-          id: 'y-axis-2',
-          type: 'linear',
-          display: true,
-          position: 'right'
-        }
-      ]
-    }
-  };
+
 
 });
